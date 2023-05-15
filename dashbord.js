ed29247eda9a0b952b68023a2827be02d665d125
johnson feranmi <olamilekanferanmi99@gmail.com>
2023-05-15 11:03:52 +0100

    Note-app

--- dashbord.js
+++ dashbord.js
@@ -4,45 +4,49 @@ if (localStorage.chat) {
     chatArr = JSON.parse(localStorage.getItem("chat")) 
 }
 const add = ()=>{
-    // alert("welcome")
-    let item = {
-        title:title.value,
-        note:note.value
-    }
-    // console.log(item);
-    chatArr.push(item)
-
-    // console.log(chatArr);
-
-    localStorage.setItem("chat",JSON.stringify(chatArr));
+    if (title.value == "" && note.value == "") {
+        done.innerHTML = "Please Fill The Input"
+    } else {
+        done.innerHTML = ""
+        let item = {
+            title:title.value,
+            note:note.value
+        }
+        chatArr.push(item)
     
-    console.log(localData);
+        localStorage.setItem("chat",JSON.stringify(chatArr));
+    }
 
     title.value = "";
     note.value = "";
-    location.reload()
-}
-const displayChat = ()=>{
-    let localData = JSON.parse(localStorage.getItem("chat")) 
-    disp.innerHTML += localData.map((value , index)=>{
-        return `
-        <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
+
+    displayChat();
+};
+
+const displayChat = () => {
+    let localData = JSON.parse(localStorage.getItem("chat")) ;
+    disp.innerHTML = "";
+    localData.map((value , index) => {
+        disp.innerHTML += `
+            <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
                 <div class="h-full p-3 rounded-lg border-2 border-indigo-500 flex flex-col overflow-hidden">
-                    <!-- <button class="bg-indigo-700 text-white px-3 py-1 tracking-widest text-l absolute right-0 top-0 rounded-bl">Delete Note</button> -->
-                    <h2 class="text-lg font-normal tracking-widest ">${value.title}</h2>
-                
+                    <h2 class="text-2xl font-medium tracking-widest text-gray-900">${value.title}</h2>
                     <div class="py-2">
                         <hr>
                     </div>
                     <p class="flex items-center text-gray-600 mb-2">${value.note}</p>
-                    <button class="flex items-center mt-auto text-white bg-indigo-600 border-0 py-2 px-4 w-full focus:outline-none hover:bg-indigo-600 rounded">Delete Note
+                    <button class="flex items-center mt-auto text-white bg-indigo-700 border-0 py-2 px-4 w-full focus:outline-none hover:bg-indigo-800 rounded" onclick="deleteNote(${index})">Delete Note
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
                             <path d="M5 12h14M12 5l7 7-7 7"></path>
                         </svg>
                     </button>
                 </div>
-            </div>
-        `
-        
+            </div>`;
     });
 }
+const deleteNote = (e) => {
+    chatArr.splice(e, 1);
+    localStorage.setItem("chat", JSON.stringify(chatArr));
+    // displayChat();
+    location.reload()
+  };
--- for.js
+++ for.js
@@ -1,59 +1,61 @@
-// Retrieve chat data from localStorage
-let chatArr = JSON.parse(localStorage.getItem("chat")) || [];
+let chatArr = []; // Declare an empty array to store chat data
 
-// Function to add new note to chat and update localStorage
-const addNote = () => {
-  const title = document.getElementById("title").value;
-  const note = document.getElementById("note").value;
+// If there is chat data saved in the localStorage, parse it and store it in chatArr
+if (localStorage.chat) {
+    chatArr = JSON.parse(localStorage.getItem("chat")); 
+}
 
-  // Create new note object
-  const newNote = { title, note };
+// This function gets called when the user clicks the "Add" button to submit a new chat item
+const add = () => {
+    // Create a new chat item with the title and note entered by the user
+    let item = {
+        title: title.value,
+        note: note.value
+    };
 
-  // Add new note to chat array
-  chatArr.push(newNote);
-
-  // Store updated chat array in localStorage
-  localStorage.setItem("chat", JSON.stringify(chatArr));
-
-  // Clear input fields
-  document.getElementById("title").value = "";
-  document.getElementById("note").value = "";
-
-  // Refresh chat display
-  displayChat();
+    // Add the new chat item to chatArr and save the updated array to localStorage
+    chatArr.push(item);
+    localStorage.setItem("chat", JSON.stringify(chatArr));
+    
+    // Clear the input fields and display the updated chat data
+    title.value = "";
+    note.value = "";
+    displayChat();
 };
 
-// Function to display chat notes from localStorage
+// This function retrieves the chat data from localStorage and generates HTML markup to display it on the page
 const displayChat = () => {
-  const chatDiv = document.getElementById("chat");
-  chatDiv.innerHTML = "";
+    let localData = JSON.parse(localStorage.getItem("chat"));
+    
+    // Clear the current chat display
+    disp.innerHTML = "";
 
-  // Loop through chat array and create HTML elements for each note
-  chatArr.forEach((note, index) => {
-    const noteDiv = document.createElement("div");
-    noteDiv.className = "p-4 xl:w-1/4 md:w-1/2 w-full";
-    noteDiv.innerHTML = `
-      <div class="h-full p-3 rounded-lg border-2 border-indigo-500 flex flex-col overflow-hidden">
-        <h2 class="text-lg font-normal tracking-widest ">${note.title}</h2>
-        <div class="py-2"><hr></div>
-        <p class="flex items-center text-gray-600 mb-2">${note.note}</p>
-        <button onclick="deleteNote(${index})" class="flex items-center mt-auto text-white bg-indigo-600 border-0 py-2 px-4 w-full focus:outline-none hover:bg-indigo-600 rounded">Delete Note
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
-        </button>
-      </div>
-    `;
-    chatDiv.appendChild(noteDiv);
-  });
+    // Generate HTML markup for each chat item and append it to the chat display
+    localData.map((value , index) => {
+        disp.innerHTML += `
+            <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
+                <div class="h-full p-3 rounded-lg border-2 border-indigo-500 flex flex-col overflow-hidden">
+                    <h2 class="text-2xl font-medium tracking-widest text-gray-900">${value.title}</h2>
+                    <div class="py-2">
+                        <hr>
+                    </div>
+                    <p class="flex items-center text-gray-600 mb-2">${value.note}</p>
+                    <button class="flex items-center mt-auto text-white bg-indigo-700 border-0 py-2 px-4 w-full focus:outline-none hover:bg-indigo-800 rounded" onclick="deleteNote(${index})">Delete Note
+                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
+                            <path d="M5 12h14M12 5l7 7-7 7"></path>
+                        </svg>
+                    </button>
+                </div>
+            </div>`;
+    });
 };
 
-// Function to delete a note from the chat array and update localStorage
+// This function gets called when the user clicks the "Delete Note" button on a chat item
 const deleteNote = (index) => {
-  chatArr.splice(index, 1);
-  localStorage.setItem("chat", JSON.stringify(chatArr));
-  displayChat();
+    // Remove the chat item at the specified index from chatArr and save the updated array to localStorage
+    chatArr.splice(index, 1);
+    localStorage.setItem("chat", JSON.stringify(chatArr));
+    
+    // Display the updated chat data
+    displayChat();
 };
-
-// Call displayChat function on page load
-displayChat();
